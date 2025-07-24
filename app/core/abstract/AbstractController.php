09cf118cb282cb58451656ae6bf6d32cf1cc8f19
gorguimarena <gorguimarena@gmail.com>
2025-07-24 16:38:16 +0000

    gestion du cors

--- app/core/abstract/AbstractController.php
+++ app/core/abstract/AbstractController.php
@@ -10,8 +10,11 @@ abstract class AbstractController extends Singleton
 
     protected function renderJson(ResponseEntity $response): void
     {
-        http_response_code(201);
+        http_response_code($response->code);
 
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header('Content-Type: application/json');
 
         echo $response->toJson();
